884a18e96879556135bc908199ccc788127e2a8e
Viktoria Dobisova <viktoria.dobisova@student.kdg.be>
2025-10-05 14:43:04 +0200

    structure

---
+++ src/main/java/be/kdg/keepdishesgoing/KeepDishesGoingModulithApp.java
@@ -0,0 +1,28 @@
+package be.kdg.keepdishesgoing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.modulith.Modulith;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Modulith
+@EnableScheduling
+public class KeepDishesGoingModulithApp {
+
+    private static final Logger log = LoggerFactory.getLogger(KeepDishesGoingModulithApp.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepDishesGoingModulithApp.class, args);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    void onApplicationStarted() {
+        ApplicationModules modules = ApplicationModules.of(KeepDishesGoingModulithApp.class);
+        modules.forEach(module -> log.info("\n{}", module));
+    }
+
+}
